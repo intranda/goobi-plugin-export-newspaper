aa07e00dcb8fcb3c69bf9352606dc79094a71160
Robert Sehr <robert.sehr@intranda.com>
2024-12-09 07:26:41 +0100

    test preparation, sample files

--- module-base/src/test/java/de/intranda/goobi/plugins/NewspaperExportPluginTest.java
+++ module-base/src/test/java/de/intranda/goobi/plugins/NewspaperExportPluginTest.java
@@ -1,15 +1,28 @@
 package de.intranda.goobi.plugins;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.MatchResult;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
 import org.easymock.EasyMock;
+import org.goobi.beans.Process;
+import org.goobi.beans.Ruleset;
+import org.goobi.beans.Step;
+import org.goobi.production.enums.PluginType;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -22,10 +35,16 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.config.ConfigurationHelper;
+import de.sub.goobi.helper.VariableReplacer;
+import de.sub.goobi.metadaten.MetadatenHelper;
+import ugh.dl.Fileformat;
+import ugh.dl.Prefs;
+import ugh.fileformats.mets.MetsMods;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ ConfigPlugins.class })
-@PowerMockIgnore({ "javax.management.*", "javax.net.ssl.*", "jdk.internal.reflect.*" })
+@PrepareForTest({ MetadatenHelper.class, VariableReplacer.class, ConfigurationHelper.class, ConfigPlugins.class })
+@PowerMockIgnore({ "javax.management.*", "javax.xml.*", "org.xml.*", "org.w3c.*", "javax.net.ssl.*", "jdk.internal.reflect.*" })
 public class NewspaperExportPluginTest {
 
     @Rule
@@ -33,8 +52,14 @@ public class NewspaperExportPluginTest {
     private File tempFolder;
     private static String resourcesFolder;
 
+    private File processDirectory;
+    private File metadataDirectory;
+    private Process process;
+    private Step step;
+    private Prefs prefs;
+
     @BeforeClass
-    public static void setUpClass() throws Exception {
+    public static void setUpClass() {
         resourcesFolder = "src/test/resources/"; // for junit tests in eclipse
 
         if (!Files.exists(Paths.get(resourcesFolder))) {
@@ -55,10 +80,11 @@ public class NewspaperExportPluginTest {
         if (!Files.exists(Paths.get(resourcesFolder))) {
             resourcesFolder = "target/test-classes/"; // to run mvn test from cli or in jenkins
         }
-
         PowerMock.mockStatic(ConfigPlugins.class);
         EasyMock.expect(ConfigPlugins.getPluginConfig(EasyMock.anyString())).andReturn(getConfig()).anyTimes();
         PowerMock.replay(ConfigPlugins.class);
+        process = prepareProcess();
+
     }
 
     @Test
@@ -67,8 +93,43 @@ public class NewspaperExportPluginTest {
         assertNotNull(plugin);
     }
 
+    @Test
+    public void testPluginTitle() {
+        NewspaperExportPlugin plugin = new NewspaperExportPlugin();
+        assertEquals("intranda_export_newspaper", plugin.getTitle());
+    }
+
+    @Test
+    public void testPluginType() {
+        NewspaperExportPlugin plugin = new NewspaperExportPlugin();
+        assertEquals(PluginType.Export, plugin.getType());
+    }
+
+    @Test
+    public void testProblems() {
+        NewspaperExportPlugin plugin = new NewspaperExportPlugin();
+        assertNull(plugin.getProblems());
+    }
+
+    @Test
+    public void testExportFulltext() {
+        NewspaperExportPlugin plugin = new NewspaperExportPlugin();
+        assertFalse(plugin.isExportFulltext());
+        plugin.setExportFulltext(true);
+        assertTrue(plugin.isExportFulltext());
+
+    }
+
+    @Test
+    public void testExportImages() {
+        NewspaperExportPlugin plugin = new NewspaperExportPlugin();
+        assertFalse(plugin.isExportImages());
+        plugin.setExportImages(true);
+        assertTrue(plugin.isExportImages());
+    }
+
     private XMLConfiguration getConfig() {
-        String file = "plugin_intranda_export_sample.xml";
+        String file = "plugin_intranda_export_newspaper.xml";
         XMLConfiguration config = new XMLConfiguration();
         config.setDelimiterParsingDisabled(true);
         try {
@@ -77,6 +138,83 @@ public class NewspaperExportPluginTest {
         }
         config.setReloadingStrategy(new FileChangedReloadingStrategy());
         return config;
+    }
+
+    private Process prepareProcess() throws Exception {
+
+        // create folder structure and copy files
+        metadataDirectory = folder.newFolder("metadata");
+        processDirectory = new File(metadataDirectory + File.separator + "1");
+        processDirectory.mkdirs();
+        String metadataDirectoryName = metadataDirectory.getAbsolutePath() + File.separator;
+        Path metaSource = Paths.get(resourcesFolder, "meta.xml");
+        Path metaTarget = Paths.get(processDirectory.getAbsolutePath(), "meta.xml");
+        Files.copy(metaSource, metaTarget);
+        Path anchorSource = Paths.get(resourcesFolder, "meta_anchor.xml");
+        Path anchorTarget = Paths.get(processDirectory.getAbsolutePath(), "meta_anchor.xml");
+        Files.copy(anchorSource, anchorTarget);
+
+        PowerMock.mockStatic(ConfigurationHelper.class);
+        ConfigurationHelper configurationHelper = EasyMock.createMock(ConfigurationHelper.class);
+        EasyMock.expect(ConfigurationHelper.getInstance()).andReturn(configurationHelper).anyTimes();
+        EasyMock.expect(configurationHelper.getMetsEditorLockingTime()).andReturn(1800000l).anyTimes();
+        EasyMock.expect(configurationHelper.isAllowWhitespacesInFolder()).andReturn(false).anyTimes();
+        EasyMock.expect(configurationHelper.useS3()).andReturn(false).anyTimes();
+        EasyMock.expect(configurationHelper.isUseProxy()).andReturn(false).anyTimes();
+        EasyMock.expect(configurationHelper.getGoobiContentServerTimeOut()).andReturn(60000).anyTimes();
+        EasyMock.expect(configurationHelper.getMetadataFolder()).andReturn(metadataDirectoryName).anyTimes();
+        EasyMock.expect(configurationHelper.getRulesetFolder()).andReturn(resourcesFolder).anyTimes();
+        EasyMock.expect(configurationHelper.getConfigurationFolder()).andReturn(resourcesFolder).anyTimes();
+        EasyMock.expect(configurationHelper.getTemporaryFolder()).andReturn(tempFolder.getAbsolutePath()).anyTimes();
+        EasyMock.expect(configurationHelper.getProcessImagesMainDirectoryName()).andReturn("processtitle_media").anyTimes();
+        EasyMock.expect(configurationHelper.getProcessImagesMasterDirectoryName()).andReturn("master_processtitle_media").anyTimes();
+        EasyMock.expect(configurationHelper.getProcessOcrTxtDirectoryName()).andReturn("processtitle_txt").anyTimes();
+        EasyMock.expect(configurationHelper.getProcessOcrXmlDirectoryName()).andReturn("processtitle_xml").anyTimes();
+        EasyMock.expect(configurationHelper.getProcessOcrPdfDirectoryName()).andReturn("processtitle_pdf").anyTimes();
+        EasyMock.expect(configurationHelper.getProcessImagesSourceDirectoryName()).andReturn("processtitle_source").anyTimes();
+        EasyMock.expect(configurationHelper.getProcessImportDirectoryName()).andReturn("import").anyTimes();
+        EasyMock.expect(configurationHelper.getScriptsFolder()).andReturn(resourcesFolder).anyTimes();
+        EasyMock.expect(configurationHelper.getGoobiFolder()).andReturn(resourcesFolder).anyTimes();
+        EasyMock.replay(configurationHelper);
+
+        PowerMock.replay(ConfigurationHelper.class);
+
+        Process proc = new Process();
+        proc.setId(1);
+        proc.setTitel("process title");
+        Ruleset r = new Ruleset();
+        r.setDatei("ruleset_newspaper.xml");
+        r.setTitel("ruleset_newspaper.xml");
+        proc.setRegelsatz(r);
+
+        PowerMock.mockStatic(VariableReplacer.class);
+        EasyMock.expect(VariableReplacer.simpleReplace(EasyMock.anyString(), EasyMock.anyObject())).andReturn("master_processtitle_media");
+        EasyMock.expect(VariableReplacer.simpleReplace(EasyMock.anyString(), EasyMock.anyObject())).andReturn("processtitle_xml");
+        EasyMock.expect(VariableReplacer.simpleReplace(EasyMock.anyString(), EasyMock.anyObject())).andReturn("").anyTimes();
+        List<MatchResult> results = new ArrayList<>();
+        EasyMock.expect(VariableReplacer.findRegexMatches(EasyMock.anyString(), EasyMock.anyString())).andReturn(results).anyTimes();
+
+        PowerMock.replay(VariableReplacer.class);
+        prefs = new Prefs();
+        prefs.loadPrefs(resourcesFolder + "ruleset_newspaper.xml");
+        Fileformat ff = new MetsMods(prefs);
+        ff.read(metaTarget.toString());
+
+        PowerMock.mockStatic(MetadatenHelper.class);
+        EasyMock.expect(MetadatenHelper.getMetaFileType(EasyMock.anyString())).andReturn("mets").anyTimes();
+        EasyMock.expect(MetadatenHelper.getFileformatByName(EasyMock.anyString(), EasyMock.anyObject())).andReturn(ff).anyTimes();
+        EasyMock.expect(MetadatenHelper.getMetadataOfFileformat(EasyMock.anyObject(), EasyMock.anyBoolean()))
+                .andReturn(Collections.emptyMap())
+                .anyTimes();
+        PowerMock.replay(MetadatenHelper.class);
+
+        step = new Step();
+        step.setId(1);
+        step.setTitel("step title");
+        List<Step> steplist = new ArrayList<>();
+        steplist.add(step);
+        proc.setSchritte(steplist);
+        return proc;
     }
 
 }
